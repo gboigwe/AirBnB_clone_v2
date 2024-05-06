93f94b79be00ba3d722c017c69f7caebd351da0e
gboigwe <geakande@gmail.com>
2024-05-06 17:00:38 +0100

    Deploying static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,16 +2,38 @@
 """
 A Fabric script that distributes an archive
 to my web servers, with the do_deploy function:
+executing: fabric file:
+fab -f 3-deploy_web_static.py
+deploy to ubuntu user on server
 """
 
-
-from os.path import exists
-from fabric.api import put, run, env
+from os.path import exists, isdir
+from fabric.api import env, local, put, run
+from datetime import datetime
 env.hosts = ['52.3.240.1', '100.24.237.31']
 
 
+def do_pack():
+    """Using tgz to archive web_static files"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+
+def deploy():
+    """Created an archive file for the web_static deploment"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
+
 def do_deploy(archive_path):
-    """Archiving files to be distributed into web server"""
+    """Creating and deploying the archived file"""
     if exists(archive_path) is False:
         return False
     try:
