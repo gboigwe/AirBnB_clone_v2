5a38d3d384f5f46978fc99603c7d48546d413010
gboigwe <geakande@gmail.com>
2024-05-24 17:30:51 +0100

    script that starts a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Code to run web templateon flask with port 0.0.0.0:5000
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Display somthing in the page"""
+    return 'Hello HBNB!'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display somthing in the page"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
