ef3d1bdae0036e39ef14d92eb0841ec21445948c
gboigwe <geakande@gmail.com>
2024-05-24 17:24:31 +0100

    script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Code to run web templateon flask with port 0.0.0.0:5000
 """
-
-
 from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_w():
-    return ""<p>Hello, World</p>
+def hello():
+    """Display somthing in the page"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
